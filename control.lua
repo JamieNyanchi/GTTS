e7dee9d61654414a579b4f5e252220895d9e4842
JamieNyanchi <80130176+JamieNyanchi@users.noreply.github.com>
2025-07-24 07:31:10 -0400

    Refactor runtime map adjuster code
    Refactors the map adjuster code to use the Factorio lualib event handler.

---
+++ control.lua
@@ -0,0 +1,8 @@
+-- Global config for the mod
+require("config")
+
+-- Factorio lualib event handler
+local handler = require("__core__.lualib.event_handler")
+handler.add_libraries({
+    require("scripts.map-adjuster"),
+})
