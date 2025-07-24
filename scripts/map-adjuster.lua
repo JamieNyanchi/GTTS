dbe634ab5ac0862ed0d9df6c1f890d52e89c9ebd
JamieNyanchi <80130176+JamieNyanchi@users.noreply.github.com>
2025-07-24 07:31:10 -0400

    Refactor runtime map adjuster code
    Refactors the map adjuster code to use the Factorio lualib event handler.

--- scripts/map-adjuster.lua
+++ scripts/map-adjuster.lua
@@ -1,11 +1,13 @@
-require "config"
+-- Return data for use by the event handler
+local map_adjuster = {}
 
---This turns an array of keys below the global "game" object into
---a table reference and a key in that table as a tuple.
+
+-- This turns an array of keys below the global "game" object into
+-- a table reference and a key in that table as a tuple.
 --
---t,k = get_reference(keys)
+-- t,k = get_reference(keys)
 --
---the reference becomes t[k]
+-- the reference becomes t[k]
 local function get_reference(keys)
     local length = #keys
     local reftable = game
@@ -15,10 +17,11 @@ local function get_reference(keys)
     return reftable, keys[length]
 end
 
---Helper method for updating settings given the setting name, the
---global variable to which the previous value of that variable is
---saved, a list of keys to the variable to be changed, and a boolean
---indicating whether it is a speed or duration.
+
+-- Helper method for updating settings given the setting name, the
+-- global variable to which the previous value of that variable is
+-- saved, a list of keys to the variable to be changed, and a boolean
+-- indicating whether it is a speed or duration.
 local function update_GTTS_setting(setting, save, targetvariable, speed)
     local t, k = get_reference(targetvariable)
 
@@ -42,18 +45,19 @@ end
 
 local function updateSurfaces()
     for name, surface in pairs(game.surfaces) do
-        update_GTTS_setting("gtts-Adjust-DayNight", "initial-day-night_" .. name, { "surfaces", name, "ticks_per_day" },
-            false)
-        update_GTTS_setting("gtts-Adjust-WindSpeed", "initial-windspeed_" .. name, { "surfaces", name, "wind_speed" },
-            true)
+        update_GTTS_setting("gtts-Adjust-DayNight", "initial-day-night_" .. name,
+            { "surfaces", name, "ticks_per_day" }, false)
+        update_GTTS_setting("gtts-Adjust-WindSpeed", "initial-windspeed_" .. name,
+            { "surfaces", name, "wind_speed" }, true)
     end
 end
+
 
 local function updateMapSettings()
     update_GTTS_setting("gtts-Adjust-Pollution", "initial-pollution-diffusionratio",
         { "map_settings", "pollution", "diffusion_ratio" }, true)
-    update_GTTS_setting("gtts-Adjust-Pollution", "initial-pollution-ageing", { "map_settings", "pollution", "ageing" },
-        true)
+    update_GTTS_setting("gtts-Adjust-Pollution", "initial-pollution-ageing",
+        { "map_settings", "pollution", "ageing" }, true)
 
     update_GTTS_setting("gtts-Adjust-Evolution", "initial-evolution-timefactor",
         { "map_settings", "enemy_evolution", "time_factor" }, true)
@@ -75,44 +79,52 @@ local function updateMapSettings()
     updateSurfaces()
 end
 
---Only add events if the safe mode setting is not enabled.
-if settings.startup["gtts-z-No-Runtime-Adjustments"].value == false then
-    script.on_configuration_changed(
-        function(event)
-            storage["previous-scale"] = 1.0 / game.speed
-        end
-    )
 
-    script.on_event(defines.events.on_tick,
-        function(event)
-            --Only change the game speed if the target frame rate has changed or Reset-GameSpeed was disabled.
-            --For this we save "previous-speed" in the global table with the last adjusted game speed. This
-            --prevents the game speed from being changed if the user, or another mod, changes the game speed.
-                if ((not storage["previous-speed"]) or (not (storage["previous-speed"] == gtts_time_scale_inverse)) and game.tick > 1) then
-                    if (not storage["previous-scale"]) or (not (storage["previous-scale"] == gtts_time_scale)) then
-                        updateMapSettings()
-                        storage["previous-scale"] = gtts_time_scale
-                    end
-
-                    if settings.global["gtts-Reset-GameSpeed"].value == false then
-                        if settings.startup["gtts-Adjust-GameSpeed"].value == true then
-                            game.speed = gtts_time_scale_inverse
-                            storage["previous-speed"] = gtts_time_scale_inverse
-                        end
-                    end
-                end
-        end
-    )
-
-    script.on_event(defines.events.on_runtime_mod_setting_changed,
-        function(event)
-            if settings.global["gtts-Reset-GameSpeed"].value == true then
-                game.speed = 1.0
-                storage["previous-speed"] = 1.0
-            end
-            updateMapSettings()
-        end
-    )
-
-    script.on_event(defines.events.on_surface_created, updateSurfaces)
+local function on_configuration_changed(event)
+    storage["previous-scale"] = 1.0 / game.speed
 end
+
+
+local function on_tick(event)
+    -- Only change the game speed if the target frame rate has changed or Reset-GameSpeed was disabled.
+    -- For this we save "previous-speed" in the global table with the last adjusted game speed. This
+    -- prevents the game speed from being changed if the user, or another mod, changes the game speed.
+    if ((not storage["previous-speed"]) or (not (storage["previous-speed"] == gtts_time_scale_inverse)) and game.tick > 1) then
+        if (not storage["previous-scale"]) or (not (storage["previous-scale"] == gtts_time_scale)) then
+            updateMapSettings()
+            storage["previous-scale"] = gtts_time_scale
+        end
+
+        if settings.global["gtts-Reset-GameSpeed"].value == false then
+            if settings.startup["gtts-Adjust-GameSpeed"].value == true then
+                game.speed = gtts_time_scale_inverse
+                storage["previous-speed"] = gtts_time_scale_inverse
+            end
+        end
+    end
+end
+
+
+local function on_runtime_mod_setting_changed(event)
+    if settings.global["gtts-Reset-GameSpeed"].value == true then
+        game.speed = 1.0
+        storage["previous-speed"] = 1.0
+    end
+    updateMapSettings()
+end
+
+
+-- Only add events if the safe mode setting is not enabled.
+if settings.startup["gtts-z-No-Runtime-Adjustments"].value == false then
+    map_adjuster.on_configuration_changed = on_configuration_changed
+
+    map_adjuster.events = {
+        [defines.events.on_tick] = on_tick,
+        [defines.events.on_runtime_mod_setting_changed] = on_runtime_mod_setting_changed,
+        [defines.events.on_surface_created] = updateSurfaces,
+    }
+end
+
+
+-- Return the data table
+return map_adjuster
